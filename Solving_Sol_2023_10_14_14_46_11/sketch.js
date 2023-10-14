d6a57e1b6228ed2e33ae86a27692a9647e211037
ITIGOSWAMY <144553892+ITIGOSWAMY@users.noreply.github.com>
2023-10-14 10:52:16 -0400

    Add files via upload

---
+++ Solving_Sol_2023_10_14_14_46_11/sketch.js
@@ -0,0 +1,135 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  
+//
+  
+  fill(0)
+  stroke(0)//black
+  ellipse(200,200,600,600)
+  
+  fill(255, 255, 0)//blue
+  stroke(255, 255, 0)
+  ellipse(200,200,550,550)
+  
+  fill(0, 0, 255)//yellow
+  stroke(0, 0, 255)
+  ellipse(200,200,500,500)
+  
+  fill(255, 0, 0)//red
+  stroke(255, 0, 0)
+  ellipse(200,200,450,450)
+  
+  /
+    
+  fill(0)
+  stroke(0)//black
+  ellipse(200,200,400,400)
+
+  fill(255, 255, 0)//blue
+  stroke(255, 255, 0)
+  ellipse(200,200,350,350)
+
+  fill(0, 0, 255)//yellow
+  stroke(0, 0, 255)
+  ellipse(200,200,300,300)
+
+  fill(255, 0, 0)//red
+  stroke(255, 0, 0)
+  ellipse(200,200,250,250)
+
+  //
+
+  fill(0)
+  stroke(0)//black
+  ellipse(200,200,200,200)
+
+  fill(255, 255, 0)//blue
+  stroke(255, 255, 0)
+  ellipse(200,200,150,150)
+
+  fill(0, 0, 255)//yellow
+  stroke(0, 0, 255)
+  ellipse(200,200,100,100)
+
+  fill(255, 0, 0)//red
+  stroke(255, 0, 0)
+  ellipse(200,200,50,50)
+  
+ 
+  //
+  
+
+  fill(0)
+  stroke(0)
+  arc(50, 350, 80, 80, 0, PI + QUARTER_PI);
+  
+  fill(255, 255, 0)
+  stroke(255, 255, 0)
+  arc(50, 250, 80, 80, 0, PI + QUARTER_PI, OPEN);
+  
+  fill(0, 0, 255)
+  stroke(0, 0, 255)
+  arc(50, 150, 80, 80, 0, PI + QUARTER_PI, CHORD);
+  
+  fill(255, 0, 0)
+  stroke(255, 0, 0)
+  arc(50, 50, 80, 80, 0, PI + QUARTER_PI, PIE);
+  
+//
+  
+  fill(0)
+  stroke(0)
+  arc(350, 350, 80, 80, 0, PI + QUARTER_PI);
+  
+  fill(255, 255, 0)
+  stroke(255, 255, 0)
+  arc(250, 350, 80, 80, 0, PI + QUARTER_PI, OPEN);
+  
+  fill(0, 0, 255)
+  stroke(0, 0, 255)
+  arc(150, 350, 80, 80, 0, PI + QUARTER_PI, CHORD);
+  
+  fill(255, 0, 0)
+  stroke(255, 0, 0)
+  arc(50, 350, 80, 80, 0, PI + QUARTER_PI, PIE);
+  
+ //
+  
+  fill(0)
+  stroke(0)
+  arc(350, 50, 80, 80, 0, PI + QUARTER_PI);
+  
+  fill(255, 255, 0)
+  stroke(255, 255, 0)
+  arc(250, 50, 80, 80, 0, PI + QUARTER_PI, OPEN);
+  
+  fill(0, 0, 255)
+  stroke(0, 0, 255)
+  arc(150, 50, 80, 80, 0, PI + QUARTER_PI, CHORD);
+  
+  fill(255, 0, 0)
+  stroke(255, 0, 0)
+  arc(50, 50, 80, 80, 0, PI + QUARTER_PI, PIE);
+  
+//
+  
+  fill(0)
+  stroke(0)
+  arc(350, 350, 80, 80, 0, PI + QUARTER_PI);
+  
+  fill(255, 255, 0)
+  stroke(255, 255, 0)
+  arc(350, 250, 80, 80, 0, PI + QUARTER_PI, OPEN);
+  
+  fill(0, 0, 255)
+  stroke(0, 0, 255)
+  arc(350, 150, 80, 80, 0, PI + QUARTER_PI, CHORD);
+  
+  
+
+}
