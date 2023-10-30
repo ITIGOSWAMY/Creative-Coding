5246a6c426deb50d05bc783ebc28b9ee31940690
ITIGOSWAMY <144553892+ITIGOSWAMY@users.noreply.github.com>
2023-10-30 17:06:25 -0400

    Add files via upload

---
+++ Harvard_Art_Museum_Data_2023_10_30_21_01_17/sketch.js
@@ -0,0 +1,32 @@
+let apiKey = 'eec85815-ab2e-465c-acab-f168f2edfb1f'; 
+let artworks = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  generateNewArtwork();
+}
+
+function draw() {
+
+}
+
+function mousePressed() {
+  generateNewArtwork(); 
+}
+
+async function generateNewArtwork() {
+  let request = await fetch(`https://api.harvardartmuseums.org/object?apikey=${apiKey}&size=50`); 
+  let data = await request.json();
+
+  artworks = data.records.filter(artwork => artwork.primaryimageurl);
+
+  if (artworks.length > 0) {
+    let randomIndex = Math.floor(random(artworks.length));
+    let selectedArtwork = artworks[randomIndex];
+
+    createImg(selectedArtwork.primaryimageurl, "artwork").position(0, 0);
+
+    console.log("Title:", selectedArtwork.title);
+    
+  }
+}
